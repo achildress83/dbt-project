0b16cdf4914e21d05992cc1702e1e13dd5abb9a6
Aaron Childress <aaronlchildress@gmail.com>
2024-08-31 23:20:31 +0000

    add stg_stripe_payments and fct_orders

---
+++ models/staging/stripe/stg_stripe__payments.sql
@@ -0,0 +1,8 @@
+select
+    id as payment_id
+    ,orderid as order_id
+    ,paymentmethod as payment_method
+    ,status
+    ,amount/100 as amount -- convert to dollars
+    ,created as created_at
+from `dbt-tutorial`.stripe.payment
